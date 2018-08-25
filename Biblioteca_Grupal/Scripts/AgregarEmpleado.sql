2db4cf67977145fddd5198ff75ef2bd40cbf5a38
NILTON EDY PEREZ MAMANI <42424757+perez10@users.noreply.github.com>
2018-08-25 12:48:54 -0500

    Add files via upload
    ScriptAgregarEmpleado

---
+++ Biblioteca_Grupal/Scripts/AgregarEmpleado.sql
@@ -0,0 +1,31 @@
+﻿/*
+Plantilla de script posterior a la implementación							
+--------------------------------------------------------------------------------------
+ Este archivo contiene instrucciones de SQL que se anexarán al script de compilación.		
+ Use la sintaxis de SQLCMD para incluir un archivo en el script posterior a la implementación.			
+ Ejemplo:      :r .\miArchivo.sql								
+ Use la sintaxis de SQLCMD para hacer referencia a una variable en el script posterior a la implementación.		
+ Ejemplo:      :setvar TableName miTabla							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+INSERT INTO Empleado values('TICONA CALCINA JHULYUS ANTHONY','Tacna','920451214','ticona@gmail.com'),
+							('QUISPE CORI RONALD DAVID','Tacna','920451215','david@gmail.com'),
+							('CALIZAYA ZAPANA MARCO ANTONIO','Tacna','920451216','antonio@gmail.com'),
+							('FIGUEROA LAURA ALEXIS VALENTIN','Tacna','920451217','valentin@gmail.com'),
+							('QUENTA LLANO REBECA MERCEDES','Tacna','920451218','rebeca@gmail.com'),
+							('GUTIERREZ TAPIA ALEXANDER','Tacna','920451219','alexander@gmail.com'),
+							('MAMANI GALLEGOS MIGUEL','Tacna','920451220','miguel@gmail.com'),
+							('NINA VELAZCO NEIL','Tacna','920451221','neil@gmail.com'),
+							('CONDORI QUIÑONEZ WILMER','Tacna','920451222','wilmer@gmail.com'),
+							('PINTO MAMANI YENI','Tacna','920451223','yeni@gmail.com'),
+							('APAZA OCHOA ELIAS','Tacna','920451224','elias@gmail.com'),
+							('TELLEZ BEIZAGA GABRIELA','Tacna','920451225','gabriela@gmail.com'),
+							('QUISPE COAQUIRA ODETH','Tacna','920451226','odeth@gmail.com'),
+							('LARICO CHURA WINNIE','Tacna','920451227','winnie@gmail.com'),
+							('ESTRADA CARDENAS FIORELLA','Tacna','920451228','fiorella@gmail.com'),
+							('AQUERA PAQUITA ALEX WILLIAM','Tacna','920451229','william@gmail.com'),
+							('CHURA MAMANI LUIS FERNANDO','Tacna','920451230','luis@gmail.com'),
+							('MAMANI OLIVERA ANEL DIANA','Tacna','920451231','anel@gmail.com'),
+							('DIAZ PILCO GABI INDIRA','Tacna','920451232','gabi@gmail.com'),
+							('TICONA RAMIREZ ALEX','Tacna','920451233','alex@gmail.com')
