4ff8cc9e48955507c960c2ab295bf215a102c77e
JhonPeterAguilarAtencio <42424870+JhonPeterAguilarAtencio@users.noreply.github.com>
2018-08-25 13:27:48 -0500

    Autor : Jhon Peter Aguilar Atencio

---
+++ Biblioteca_Grupal/Scripts/TABLA_ESTUDIANTES.sql
@@ -0,0 +1,34 @@
+USE BDBIBLIOTECA
+GO
+
+INSERT INTO ESTUDIANTE (IDESTUDIANTE,NOMBRE,DIRECCION,TELEFONO,CORREO)
+VALUES ('Guimer Coaquira', 'Ahmedabad', '925385410', 'Coaquira@gmail.com');
+VALUES ('Jhon Aguilar', 'Calle Granada S/N', '925385411', 'xxjhonxxaguilarxx@gmail.com');
+VALUES ('Edy Lopez', 'Calle Grau 25', '925385412', 'Lopez@gmail.com' );
+VALUES ('Maria Mamani', 'Av.Lima', '925385413', 'Maria@gmail.com' );
+VALUES ('Messi Leonell', 'Av Arica', '925385414', 'Messi@gmail.com' );
+VALUES ('Ronaldo Ramirex', 'Calle Venezuela', '925385415', 'Ronaldo@gmail.com' );
+VALUES ('Neymar Calizaya', 'Calle Brazil', '925385416', 'Neymar@gmail.com' );
+VALUES ('Carla Torrillo', 'Av. Lima 245', '925385417', 'Carla@gmail.com' );
+VALUES ('Ramiro Kora', 'Calle Mercedes 101', '925385418', 'Ramiro@gmail.com' );
+VALUES ('Londra Mendez', 'Asoc. viv. 1204', '925385419', 'Londra@gmail.com' );
+VALUES ('Ana Lopez', 'Alamedaz 011', '925385420', 'Ana@gmail.com' );
+VALUES ('Sara Tilsa', 'Mirador 125', '925385421', 'Sara@gmail.com' );
+VALUES ('Sarahi Coaquira', 'Rita Sur', '925385422', 'Sarahi@gmail.com' );
+VALUES ('Pamela Rubio', 'Av. Tacna 223', '925385423', 'Pamela@gmail.com' );
+VALUES ('Kyra Mendoza', 'Leguia 1225', '925385424', 'Kyra@gmail.com' );
+VALUES ('Pedro Prieto', 'Av. Lima 123', '925385425', 'Pedro@gmail.com' );
+VALUES ('David Villa', '2 Mayo 1022', '925385426', 'xxjhonxxaguilarxx@gmail.com' );
+VALUES ('Luis Suerez', 'Av. Arequipa 152', '925385427', 'David@gmail.com' );
+VALUES ('Ricardo Lopez', 'Tacna 1203', '925385428', 'Ricardo@gmail.com' );
+VALUES ('Krey Mani', 'Av. Lima 1256', '925385429', 'Krey@gmail.com' );
+VALUES ('Jean Piero', 'Av. Cusco 12324', '925385430', 'Jean@gmail.com' );
+VALUES ('Renzo Mansilla', 'Sur del Tacna 12456', '925385431', 'Renzo@gmail.com' );
+VALUES ('Tilsa Guerra', 'Urba. 2012', '925385432', 'Tilsa@gmail.com' );
+VALUES ('Millet Figueroa', 'Bolognesi nro 122', '925385433', 'Millet@gmail.com' );
+VALUES ('Paulo Londra', 'Caceres 2002', '925385434', 'Paulo@gmail.com' );
+VALUES ('Jaime Tesillo', 'Mariscal 120', '925385435', 'Jaime@gmail.com' );
+VALUES ('Rosa Mamani', 'Lutio 223', '925385436', 'Rosa@gmail.com' );
+VALUES ('Cesar Tesillo', 'Bolognesi 222', '925385437', 'Tesillo@gmail.com' );
+VALUES ('Orestes Ramirez', 'Ricardo Av.222', '925385438', 'Orestes@gmail.com' );
+VALUES ('Rafael Roto', 'Urba. 2002', '925385439', 'Rafael@gmail.com' );
