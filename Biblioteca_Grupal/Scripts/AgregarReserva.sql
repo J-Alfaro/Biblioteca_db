fde033165e06f7e760e5b5567d4364c65fad8e36
katerinMQ <42495847+katerinMQ@users.noreply.github.com>
2018-08-25 13:29:14 -0500

    reserva

---
+++ Biblioteca_Grupal/Scripts/AgregarReserva.sql
@@ -0,0 +1,37 @@
+﻿/*
+Plantilla de script posterior a la implementación							
+--------------------------------------------------------------------------------------
+ Este archivo contiene instrucciones de SQL que se anexarán al script de compilación.		
+ Use la sintaxis de SQLCMD para incluir un archivo en el script posterior a la implementación.			
+ Ejemplo:      :r .\miArchivo.sql								
+ Use la sintaxis de SQLCMD para hacer referencia a una variable en el script posterior a la implementación.		
+ Ejemplo:      :setvar TableName miTabla							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+insert into dbo.RESERVA values ('1','21','2018-03-04'),
+								('1','22','2018-04-12'),
+								('1','23','2018-07-10'),
+								('1','24','2018-05-20'),
+								('1','25','2018-05-14'),
+								('1','26','2018-07-04'),
+								('1','27','2018-05-04'),
+								('1','28','2018-08-04'),
+								('1','29','2018-05-04'),
+								('1','30','2018-05-04'),
+								('1','31','2018-05-04'),
+								('1','32','2018-05-04'),
+								('1','33','2018-05-04'),
+								('1','34','2018-05-04'),
+								('1','35','2018-05-04'),
+								('1','36','2018-05-04'),
+								('1','37','2018-05-04'),
+								('1','38','2018-05-04'),
+								('1','39','2018-05-04'),
+								('1','40','2018-09-14'),
+								('1','41','2018-05-04'),
+								('1','42','2018-09-04'),
+								('1','43','2018-05-04'),
+								('1','44','2018-05-04'),
+								('1','45','2018-05-04')
